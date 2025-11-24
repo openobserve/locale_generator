001ec226757416444e9ac7ea41c2a337d56909c5
Bhargav <BJP232004@GMAIL.COM>
2025-11-24 18:27:29 +0530

    fix: partial language file generation

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-from translator import CreateTranslationFile
+from translator import CreateOpenSourceTranslationFile
 
 # Get the list of languages at - https://docs.aws.amazon.com/translate/latest/dg/what-is-languages.html
 
-CreateTranslationFile("tr")  # Turkish
-CreateTranslationFile("fr")  # French
-CreateTranslationFile("es")  # Spanish
-CreateTranslationFile("hi")  # Hindi
+# CreateTranslationFile("tr")  # Turkish
 # CreateTranslationFile("zh")  # Chinese
+# CreateTranslationFile("fr")  # French
+# CreateTranslationFile("es")  # Spanish
 # CreateTranslationFile("de")  # German
 # CreateTranslationFile("it")  # Italian
 # CreateTranslationFile("pt")  # Portuguese
 # CreateTranslationFile("ja")  # Japanese
 # CreateTranslationFile("ko")  # Korean
+# CreateTranslationFile("nl")  # Dutch
+
+CreateOpenSourceTranslationFile("tr")  # Turkish
+CreateOpenSourceTranslationFile("zh")  # Chinese
+CreateOpenSourceTranslationFile("fr")  # French
+CreateOpenSourceTranslationFile("es")  # Spanish
+CreateOpenSourceTranslationFile("de")  # German
+CreateOpenSourceTranslationFile("it")  # Italian
+CreateOpenSourceTranslationFile("pt")  # Portuguese
+CreateOpenSourceTranslationFile("ja")  # Japanese
+CreateOpenSourceTranslationFile("ko")  # Korean
+CreateOpenSourceTranslationFile("nl")  # Dutch
+# CreateOpenSourceTranslationFile("hi")  # Hindi
+
+
+# CreateTranslationFile("hi")  # Hindi
 # CreateTranslationFile("ar")  # Arabic
 # CreateTranslationFile("ru")  # Russian
 # CreateTranslationFile("sv")  # Swedish
 # CreateTranslationFile("da")  # Danish
 # CreateTranslationFile("fi")  # Finnish
 # CreateTranslationFile("no")  # Norwegian
-# CreateTranslationFile("nl")  # Dutch
 # CreateTranslationFile("pl")  # Polish
 # CreateTranslationFile("cs")  # Czech
 # CreateTranslationFile("el")  # Greek
